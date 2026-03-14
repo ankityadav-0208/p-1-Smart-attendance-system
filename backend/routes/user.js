2b187d2420f1f186d481a210be1829da5ca6c07f
Ankityadav <vankityadav02@gmail.com>
2026-03-14 14:49:37 +0530

    Fix model imports to use lowercase user

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const User = require('../models/User');
+const User = require('../models/user');
 const { protect, authorize } = require('../middleware/auth');
 
 // @desc    Get all users (admin only)
